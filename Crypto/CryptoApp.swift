a6b60ed6f3d7fc69eeb418be9d9a9be11643d6d8
thaxz <thais.monteiroc@ufpe.br>
2023-01-02 17:19:20 -0300

    first commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by thaxz on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
